1c3b97204cec34680725dc57d5201ca642f01b94
ciceropzr <ciceropzr@gmail.com>
2017-12-11 10:24:57 -0200

    dezembro 2

--- Reciclação/src/components/AcoesComponents/Encontros.vue
+++ Reciclação/src/components/AcoesComponents/Encontros.vue
@@ -1,32 +1,59 @@
 <template>
-	<section>
-		<div>
-			<h1>Encontros com Escolas</h1>
-			<p>Para o sucesso da ação no território, conectar os espaços de educação é para nós a garantia do sucesso. Dialogando com coordenação, direção, professores e alunos (muitos destes moradores do Morro dos Prazeres). 
-			Tais espaços são a Creche Municipal José Marinho de Oliveira, Centro de Desenvolvimento e Educação Integrada Amália Fernandes Conde (Casarão dos Prazeres), Creche Cantinho Feliz, E.M. Julia Lopes de Almeida, E.M. Machado de Assis e C.E. Monteiro de Carvalho</p>
-		</div>
-		<rd-swipe :swipe="swipe">
-         <div 
-          class="rd-swipe-item" :style="{ 'background-image': `url(${img})` }" v-for="(img, index) in imgs">
-         </div>
+  <div id="app">
+    <div class="swiper-box">
+      <div class="contacts">
+        <div 
+          class="contact"
+          :class="{ 'active': index === swipe.activeIndex }" 
+          v-for="(contact, index) in contacts"
+          @click="turnTo(index)"
+        >
+          {{contact.text}}
+        </div>
+      </div>
+      <rd-swipe :swipe="swipe">
+        <div class="rd-swipe-item" :style="{ 'background-image': `url(${img})` }" v-for="(img, index) in imgs">
+        </div>
       </rd-swipe>
-	</section>
+    </div>
+  </div>
 </template>
 
 <script>
-import rdSwipe from 'vue-slide'
+import rdSwipe from './Slide.vue'
 export default {
   data () {
     return {
       swipe: {
         activeIndex: 0
       },
+      contacts: [{
+        text: 'Page 1'
+      }, {
+        text: 'Page 2'
+      }, {
+        text: 'Page 3'
+      }, {
+        text: 'Page 4'
+      }],
       imgs: [
         'http://covteam.u.qiniudn.com/test18.jpg',
         'http://covteam.u.qiniudn.com/test19.jpg',
         'http://covteam.u.qiniudn.com/test20.jpg',
         'http://covteam.u.qiniudn.com/test21.jpg'
       ]
+    }
+  },
+  components: {
+    rdSwipe
+  },
+  methods: {
+    turnTo (index) {
+      this.$children.map(swipe => {
+          if (swipe.turnTo) {
+              swipe.turnTo(index)
+          }
+      })
     }
   }
 }
